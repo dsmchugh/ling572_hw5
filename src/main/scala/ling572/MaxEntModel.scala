f410f02d87ebe2c142f42fb847dd7695d3439e06
chris <cmc3c@uw.edu>
2013-02-11 16:17:11 -0500

    stupid bug

--- src/main/scala/ling572/MaxEntModel.scala
+++ src/main/scala/ling572/MaxEntModel.scala
@@ -74,7 +74,7 @@ class MaxEntModel {
     val scores = scoreInstance(instance)
     val map = new java.util.HashMap[String,java.lang.Double]()
     scores._2.foreach(kv => map.put(kv._1, kv._2))
-    scores
+    map
   }
 
 }
